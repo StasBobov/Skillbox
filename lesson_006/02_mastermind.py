6cc7920e21132cd25b723d86f46446d02846b113
Stasyan <chousetheone@gmail.com>
2022-05-10 14:29:41 +0300

    ИИ работает

--- lesson_006/02_mastermind.py
+++ lesson_006/02_mastermind.py
@@ -2,54 +2,32 @@ import sys
 import urllib.request
 from termcolor import cprint, colored
 from mastermind_engine import make_number, test_number
+from i_i import guess_number, get_result
 import time
-
-cprint('ДОБРО ПОЖАЛОВАТЬ, ЖЕЛЕЗЯКА!\n', color='red')
-time.sleep(1)
-cprint('ЗАВОДИ ИГРУЛЮ, ДВОИЧНЫЙ!\n', color='green')
-
 
 counter = 0
 program_color = 'blue'
+i_i_color = 'green'
 
-def new_game(again):
-    if again == 1:
-        make_number()
-        time.sleep(1)
-        cprint('      ИГРА НАЧАЛАСЬ!!!\n', color='red')
-        time.sleep(1)
-        cprint('Я загадал четырёхзначное число, попробуй отгадать его\n', color=program_color)
-        time.sleep(1)
-    elif again == 0:
-        sys.exit()
 
-new_game(1)
+cprint('ДОБРО ПОЖАЛОВАТЬ, ЖЕЛЕЗЯКА!\n', color='red')
+time.sleep(1)
+cprint('ЗАВОДИ ИГРУЛЮ, ДВОИЧНЫЙ!\n', color=i_i_color)
+time.sleep(1)
+cprint('      ИГРА НАЧАЛАСЬ!!!\n', color='red')
+time.sleep(1)
+cprint('Я загадал четырёхзначное число, попробуй отгадать его\n', color=program_color)
+time.sleep(1)
 
 
 while True:
-    if_number = input(colored('Введите четырёхзначное число с неповторяющимися цифрами: ', color=program_color))
-    if not if_number.isdigit():
-        cprint('Вы не ввели число, попробуйте ещё раз', color=program_color)
-        continue
-    if len(if_number) != 4:
-        print('Надо ввести 4 неповторяющиеся цифры, попробуйте ещё раз', color=program_color)
-        continue
-    number_set = set(if_number)
-    if len(number_set) != 4:
-        print('Цифры не должны повторяться, попробуйте ещё раз', color=program_color)
-        continue
+    if_number = guess_number()
+    cprint('Попробуй ка число: {0}'.format(if_number), color=i_i_color)
     counter += 1
     res = test_number(if_number)
     if res['bulls'] == 4:
         cprint('ПОЗДРАВЛЯЮ, ПОБЕДА ЗА {0} ХОДОВ!'.format(counter), color='red')
-        counter = 0
-        again = 'I will ask'
-        while True:
-            again = input(colored('Хотите ещё партию? Да - 1, нет - 0 ',  color=program_color))
-            if again == '1':
-                new_game(int(again))
-                break
-            elif again == '0':
-                new_game(int(again))
+        sys.exit()
     else:
-        print('быки - {0}, коровы - {1}'.format(res['bulls'], res['cows']))
+        get_result(res, if_number)
+        cprint('быки - {0}, коровы - {1}'.format(res['bulls'], res['cows']), color=program_color)
--- lesson_006/i_i.py
+++ lesson_006/i_i.py
@@ -7,16 +7,17 @@ all_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 non_number_list = []
 number_list = []
 
-def make_number():
-    if number_list == []:
-        correct_number = all_numbers.pop(randint(1, 9))
+def guess_number():
+    global all_numbers
+    i_guess_number = all_numbers.pop(randint(1, len(all_numbers)-1))
     for i in range(3):
         next_number = all_numbers.pop(randint(0, len(all_numbers)-1))
-        correct_number = str(correct_number) + str(next_number)
-    return correct_number
+        i_guess_number = str(i_guess_number) + str(next_number)
+    return i_guess_number
 
-def get_result():
-    pass
-
-
-print(make_number())
+def get_result(result, last_try):
+    global all_numbers
+    if (result['bulls'] + result['cows']) !=  0:
+        for i in last_try:
+            all_numbers.append(int(i))
+    all_numbers.sort()
