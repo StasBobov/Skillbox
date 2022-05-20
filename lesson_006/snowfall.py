8647a1b078afff1843f86c236aa1994ae1cb10bb
StasBobov <chousetheone@gmail.com>
2022-05-20 10:25:02 +0300

    чтото не получается

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -84,11 +84,19 @@ def let_it_snow(screen, N, color):
             l_list.append(randint(10, 30))
         new = 0
         b = 1
+    # рисуем снежинки, которые в воздухе
     for i in range(len(x_list)):
         x = x_list[i]
         y = y_list[i]
         l = l_list[i]
         snowflake(screen, color, center=(x, y), length=l)
+    if snowdriftx != []:
+        for i in range(len(snowdriftx)):
+            x = snowdriftx[i]
+            y = snowdrifty[i]
+            l = snowdriftl[i]
+            snowflake(screen, color, center=(x, y), length=l)
+
 
     # for i in range(N):
     #     if y_list[i] >= 600 - l_list[i]:
@@ -131,29 +139,31 @@ def under():
     global y_list, falling_snowflakes, a, snowdriftx, snowdrifty, snowdriftl
     for i in range(len(y_list)):
         if y_list[i] >= 600:
+            # Добавляем в отдельные списки упавшие снежинки
             if i+1 not in falling_snowflakes:
                 snowdriftx.append(x_list[i])
                 snowdrifty.append(y_list[i])
                 snowdriftl.append(l_list[i])
+                # Список с номерами снежинок на земле
                 falling_snowflakes.append(i+1)
                 a = 1
-    if a == 1:
-        action()
-
-def action():
-    '''Действия с упавшими снежниками'''
-    global x_list,y_list, l_list, falling_snowflakes, a, b, new
-    # f_s = list(falling_snowflakes.keys())
-    cprint("У нас есть упавшие снежинки {0}".format(falling_snowflakes), color=blue)
-    act = input(colored(' Что будем делать? \n 1 - удалить снежинки \n '
-                        '2 - добавить ещё снежинок \n 3 - и так сойдёт...\n ', color=blue))
-    if act == '1':
+    if snowdriftx != []:
         for i in snowdriftx:
             x_list.remove(i)
         for j in snowdrifty:
             y_list.remove(j)
         for k in snowdriftl:
             l_list.remove(k)
+    if a == 1:
+        action()
+
+def action():
+    '''Действия с упавшими снежниками'''
+    global x_list,y_list, l_list, falling_snowflakes, a, b, new
+    cprint("У нас есть упавшие снежинки {0}".format(falling_snowflakes), color=blue)
+    act = input(colored(' Что будем делать? \n 1 - удалить снежинки \n '
+                        '2 - добавить ещё снежинок \n 3 - и так сойдёт...\n ', color=blue))
+    if act == '1':
         snowdriftx.clear(), snowdrifty.clear(), snowdriftl.clear(), falling_snowflakes.clear()
     elif act == '2':
         b = 2
